0aec63bebd94fabddf58b8baf43416f89dc08773
Davidrc02 <davidrc3po@gmail.com>
2023-06-19 19:46:13 +0200

    tomcat anidado

--- DAVGAMES_Backend_David_Rodriguez_Cano/src/main/java/com/david/tfg/DavgamesBackendDavidRodriguezCanoApplication.java
+++ DAVGAMES_Backend_David_Rodriguez_Cano/src/main/java/com/david/tfg/DavgamesBackendDavidRodriguezCanoApplication.java
@@ -2,12 +2,18 @@ package com.david.tfg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DavgamesBackendDavidRodriguezCanoApplication {
+public class DavgamesBackendDavidRodriguezCanoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DavgamesBackendDavidRodriguezCanoApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DavgamesBackendDavidRodriguezCanoApplication.class);
+	}
 }
